75c465fae975d0530ddb3b5ead772f93636e171e
jh <jh@jhui-MacBookAir.local>
2025-06-05 21:44:28 +0900

    foodData answer 값 수정

--- src/lib/foodData.js
+++ src/lib/foodData.js
@@ -4,7 +4,7 @@ const foodData = [
     ImageUrl: "/옛날제품사진/짜장면.jpg",
     price: 500,
     type: "food",
-    answer: 4
+    answer: 4 
   },
   {
     id: 2,
@@ -18,91 +18,91 @@ const foodData = [
     ImageUrl: "/옛날제품사진/김밥.jpg",
     price: 300,
     type: "food",
-    answer: 4
+    answer: 3 
   },
   {
     id: 4,
     ImageUrl: "/옛날제품사진/떡볶이.jpg",
     price: 200,
     type: "food",
-    answer: 4
+    answer: 2 
   },
   {
     id: 5,
     ImageUrl: "/옛날제품사진/붕어빵.jpg",
     price: 50,
     type: "food",
-    answer: 2
+    answer: 1 
   },
   {
     id: 6,
     ImageUrl: "/옛날제품사진/신라면.jpg",
     price: 100,
     type: "food",
-    answer: 4
+    answer: 1
   },
   {
     id: 7,
     ImageUrl: "/옛날제품사진/안성탕면.jpg",
     price: 120,
     type: "food",
-    answer: 4
+    answer: 2
   },
   {
     id: 8,
     ImageUrl: "/옛날제품사진/너구리.jpg",
     price: 200,
     type: "food",
-    answer: 4
+    answer: 2
   },
   {
     id: 9,
     ImageUrl: "/옛날제품사진/월드콘.jpg",
     price: 300,
     type: "food",
-    answer: 4
+    answer: 3
   },
   {
     id: 10,
     ImageUrl: "/옛날제품사진/누가바.jpg",
     price: 30,
     type: "food",
-    answer: 2
+    answer: 1
   },
   {
     id: 11,
     ImageUrl: "/옛날제품사진/더위사냥.jpg",
     price: 200,
     type: "food",
-    answer: 4
+    answer: 2
   },
   {
     id: 12,
     ImageUrl: "/옛날제품사진/홈런볼.jpg",
     price: 300,
     type: "food",
-    answer: 4
+    answer: 3
   },
   {
     id: 13,
     ImageUrl: "/옛날제품사진/고래밥.jpg",
     price: 200,
     type: "food",
-    answer: 4
+    answer: 2
   },
   {
     id: 14,
     ImageUrl: "/옛날제품사진/맛동산.jpg",
     price: 90,
     type: "food",
-    answer: 3
+    answer: 1
   },
   {
     id: 15,
     ImageUrl: "/옛날제품사진/새우깡.jpg",
     price: 50,
     type: "food",
-    answer: 2
+    answer: 1
   }
 ];
 
