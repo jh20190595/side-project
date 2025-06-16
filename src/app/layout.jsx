b24ea79f7b0ccf0af1027e6fcf3d864fc470207b
jh <jehyug0212@gmail.com>
2025-06-16 14:08:14 +0900

    오타 수정

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 export const metadata = {
   title : '가격 맞추기 퀴즈',
-  decription : '금전 감각 테스트',
+  description : '금전 감각 테스트',
   other : {
     'google-site-verification' : 'I1t5Ha8W3oSgUN13s0D-j9oE1IHWTB5-vPD4dTq_n3A'
   },
