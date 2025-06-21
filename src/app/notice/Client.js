f4584a2d2ca644caeb1f40c3fe2b4bfa87bc45c1
jh <jehyug0212@gmail.com>
2025-06-21 17:32:24 +0900

    notice 수정

---
+++ src/app/notice/Client.js
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './page.module.css';
+
+export default function NoticeClient() {
+
+    const [IsActive, setIsActive ] = useState(false);
+
+    return (
+        <div className={styles.NoticeContainer}>
+
+            <div className={styles.NoticeTop}>
+                <h2>📢 공지사항 </h2>
+            </div>
+            <ul className={styles.NoticeWrap}>
+                <li className={styles.NoticeItem}>&apos;howmuch&apos;는 당신의 가격 감각을 테스트하는 웹사이트입니다.</li>
+
+                <li className={styles.NoticeItem} onClick={ () => setIsActive(prev => !prev)}><strong>앞으로 추가될 예정이에요!</strong>
+                { IsActive && (
+                    <ul>
+                        <li className={styles.NoticeSubItem}>- 과자 맞추기 </li>
+                        <li className={styles.NoticeSubItem}>- 지역별/브랜드별 가격 비교 퀴즈</li>
+                    </ul>
+                )}
+                </li>
+            </ul>
+        </div>
+    )
+}
--- src/app/notice/page.js
+++ src/app/notice/page.js
@@ -1,7 +1,5 @@
-'use client';
 
-import { useState } from 'react';
-import styles from './page.module.css';
+import NoticeClient from "./Client";
 
 export const metadata = {
     title : 'howmuch - 공지사항',
@@ -10,26 +8,7 @@ export const metadata = {
 
 export default function NoticePage() {
 
-    const [IsActive, setIsActive ] = useState(false);
-
     return (
-        <div className={styles.NoticeContainer}>
-
-            <div className={styles.NoticeTop}>
-                <h2>📢 공지사항 </h2>
-            </div>
-            <ul className={styles.NoticeWrap}>
-                <li className={styles.NoticeItem}>&apos;howmuch&apos;는 당신의 가격 감각을 테스트하는 웹사이트입니다.</li>
-
-                <li className={styles.NoticeItem} onClick={ () => setIsActive(prev => !prev)}><strong>앞으로 추가될 예정이에요!</strong>
-                { IsActive && (
-                    <ul>
-                        <li className={styles.NoticeSubItem}>- 과자 맞추기 </li>
-                        <li className={styles.NoticeSubItem}>- 지역별/브랜드별 가격 비교 퀴즈</li>
-                    </ul>
-                )}
-                </li>
-            </ul>
-        </div>
+        <NoticeClient />
     )
 }
